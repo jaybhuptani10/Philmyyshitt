9a9f1c5f6d4db81d70b80f919a9da49565a46182
unknown <jaybhuptani1054@gmail.com>
2024-06-17 16:53:45 +0530

    link change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import LandingPage from "./Pages/LandingPage/LandingPage";
 import Movie from "./Pages/Movie";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// axios.defaults.baseURL = "https://philmyshitt-backend.vercel.app/";
-axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "https://philmyshitt-backend.vercel.app/";
+// axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.withCredentials = true;
 const App = () => {
   return (
